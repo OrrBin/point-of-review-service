dd1b5c299f22e5fbbe9dcb39f6d4d818fa53bca7
Orr Benyamini <orr.bob@hotmail.com>
2019-12-22 19:27:45 +0200

    Removed redundant class

--- src/main/java/me/pointofreview/persistence/IMongoDataStore.java
+++
@@ -1,9 +0,0 @@
-//package me.pointofreview.persistence;
-//
-//import me.pointofreview.core.objects.CodeSnippet;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//
-//public interface IMongoDataStore extends MongoRepository<CodeSnippet, String> {
-//
-//    public CodeSnippet findBySnippetId(String snippetId);
-//}
